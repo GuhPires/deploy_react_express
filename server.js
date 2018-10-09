8b14d77d7c24b16415a46a72b421c1cef5381d8a
Gustavo <gustavo.goncalvespires@gmail.com>
2018-10-09 16:45:34 -0300

    Build Script + server static

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const port = process.env.PORT || 5000;
 
 // create a GET route
 app.get('/api', (req, res) => {
-  res.send({ express: 'Express connected to React' });
+  res.send({ express: 'Express Connected to React' });
 });
 
 // If the app is in Production
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(__dirname + "client/build"));
+    app.use(express.static(__dirname + "../"));
 
     // Handling SPA
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/client/build/index.html'));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '../index.html'));
 }
 
 // console.log that your server is up and running
